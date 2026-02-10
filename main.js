993ffd679948efb12d775b9a726a3326e71c4338
Dhyan <248529514+Dhyan-JPatel@users.noreply.github.com>
2026-02-10 14:54:47 -0500

    Update Firebase configuration in main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+'use strict';
+
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB60cn2DLhu_VzhprtZ5x_SSzfcfjKzfVk",
-  authDomain: "civilization-game-efa6b.firebaseapp.com",
-  databaseURL: "https://civilization-game-efa6b-default-rtdb.firebaseio.com",
-  projectId: "civilization-game-efa6b",
-  storageBucket: "civilization-game-efa6b.firebasestorage.app",
-  messagingSenderId: "786254154239",
-  appId: "1:786254154239:web:9320036b347a64f9567fca"
+  apiKey: 'AIzaSyB60cn2DLhu_VzhprtZ5x_SSzfcfjKzfVk',
+  authDomain: 'civilization-game-efa6b.firebaseapp.com',
+  databaseURL: 'https://civilization-game-efa6b-default-rtdb.firebaseio.com',
+  projectId: 'civilization-game-efa6b',
+  storageBucket: 'civilization-game-efa6b.firebasestorage.app',
+  messagingSenderId: '786254154239',
+  appId: '1:786254154239:web:9320036b347a64f9567fca'
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
